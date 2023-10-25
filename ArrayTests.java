716f9fe1e2643665e798b477290369ffafeacad8
gallant <ctsao@ucsd.edu>
2023-10-25 14:49:02 -0700

    Fixed array bugs

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,24 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test
+  public void customReverseInPlace() {
+    int[] input2 = {1, 2, 3, 4};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[] {4, 3, 2, 1}, input2);
+  }
+
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void customReversed(){
+    int[] input1 = {1, 2, 3};
+    int[] newArr = ArrayExamples.reversed(input1);
+    assertArrayEquals(new int[] {3, 2, 1}, newArr);
+  } 
 }
